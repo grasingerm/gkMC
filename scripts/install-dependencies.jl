07ead53542e7c6e164ccf8201bbde199a24fecfb
Matthew Grasinger <grasingerm@gmail.com>
2023-09-27 13:46:22 -0400

    added dependency

--- scripts/install-dependencies.jl
+++ scripts/install-dependencies.jl
@@ -1,7 +1,7 @@
 import Pkg
 
 for pkg in ["ParallelStencil", "StaticArrays", "PhysicalConstants", "Unitful",
-            "ArgParse", "LaTeXStrings", "Plots", "SpecialFunctions",
+            "ArgParse", "LaTeXStrings", "Plots", "PyPlot", "SpecialFunctions",
             "Profile", "PProf", "OrdinaryDiffEq", "Sundials", "MPI", 
             "Distributions"]
     @show pkg
