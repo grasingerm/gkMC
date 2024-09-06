c30e7e7e4c3ea5d9e9b0a21bf63b9482d7135490
Matthew Grasinger <grasingerm@gmail.com>
2024-09-06 13:42:50 -0400

    converting to PythonPlot

--- scripts/install-dependencies.jl
+++ scripts/install-dependencies.jl
@@ -1,7 +1,7 @@
 import Pkg
 
 for pkg in ["ParallelStencil", "StaticArrays", "PhysicalConstants", "Unitful",
-            "ArgParse", "LaTeXStrings", "Plots", "PyPlot", "SpecialFunctions",
+            "ArgParse", "LaTeXStrings", "Plots", "PythonPlot", "SpecialFunctions",
             "Profile", "PProf", "OrdinaryDiffEq", "Sundials", "MPI", 
             "Distributions"]
     @show pkg
