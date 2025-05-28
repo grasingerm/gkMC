0393a82d63afb8cf895612c79c2a48581f784d5b
Matthew Grasinger <grasingerm@gmail.com>
2025-05-28 09:17:41 -0400

    updated dependencies

--- scripts/install-dependencies.jl
+++ scripts/install-dependencies.jl
@@ -4,7 +4,7 @@ for pkg in ["ParallelStencil", "StaticArrays", "PhysicalConstants", "Unitful",
             "ArgParse", "LaTeXStrings", "Plots", "PythonPlot", "SpecialFunctions",
             "Profile", "PProf", "OrdinaryDiffEq", "Sundials", "MPI", 
             "Distributions", "ODEInterfaceDiffEq", "Colors", "ColorSchemes",
-            "YAML", "LatinHypercubeSampling"]
+            "YAML", "LatinHypercubeSampling", "StaticArrays"]
     @show pkg
     Pkg.add(pkg)
 end
